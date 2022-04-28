c758e9a58367149bdf75c4093c96060a2ffe85b4
Mahdi Sharifi <mahdi.elu@gmail.com>
2022-04-28 12:29:41 +0430

    Change 1

--- src/main/java/com/example/simplewebapp/SimpleWebAppApplication.java
+++ src/main/java/com/example/simplewebapp/SimpleWebAppApplication.java
@@ -2,10 +2,19 @@ package com.example.simplewebapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
+@RequestMapping("/")
 public class SimpleWebAppApplication {
 
+	@GetMapping
+	public String hello(){
+		return "HELLO WORLD!";
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(SimpleWebAppApplication.class, args);
 	}
